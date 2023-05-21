f0d6f799a627b2229dba31a1f751b35a0e38ffef
CursoEGG001 <soygecko@gmail.com>
2023-05-21 13:21:45 -0300

    No pregunten...quiero seguro!

--- EExtraa03/src/Entidades/Vehiculo.java
+++ EExtraa03/src/Entidades/Vehiculo.java
@@ -31,6 +31,62 @@ public class Vehiculo {
         this.tipo = tipo;
     }
 
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getNumMotor() {
+        return numMotor;
+    }
+
+    public void setNumMotor(String numMotor) {
+        this.numMotor = numMotor;
+    }
+
+    public String getChasis() {
+        return chasis;
+    }
+
+    public void setChasis(String chasis) {
+        this.chasis = chasis;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
     public Vehiculo() {
     }
 }
--- EExtraa03/src/clase11/EExtraa03.java
+++ EExtraa03/src/clase11/EExtraa03.java
@@ -4,6 +4,14 @@
  */
 package clase11;
 
+import Entidades.Clientes;
+import Entidades.Polizas;
+import Entidades.Vehiculo;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 /**
  *
  * @author pc
@@ -14,7 +22,100 @@ public class EExtraa03 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+
+        // Crear una instancia de la clase Cliente
+        Clientes cliente = new Clientes();
+
+        // Ingresar los datos del cliente
+        System.out.println("Ingrese el nombre del cliente:");
+        cliente.setNombre(scanner.nextLine());
+
+        System.out.println("Ingrese el apellido del cliente:");
+        cliente.setApellido(scanner.nextLine());
+
+        System.out.println("Ingrese el email del cliente:");
+        cliente.setEmail(scanner.nextLine());
+
+        System.out.println("Ingrese el domicilio del cliente:");
+        cliente.setDomicilio(scanner.nextLine());
+
+        System.out.println("Ingrese el teléfono del cliente:");
+        cliente.setTelefono(scanner.nextLine());
+
+        // Crear una instancia de la clase Vehiculo
+        Vehiculo vehiculo = new Vehiculo();
+
+        // Ingresar los datos del vehículo
+        System.out.println("Ingrese la marca del vehículo:");
+        vehiculo.setMarca(scanner.nextLine());
+
+        System.out.println("Ingrese el modelo del vehículo:");
+        vehiculo.setModelo(scanner.nextLine());
+
+        System.out.println("Ingrese el año del vehículo:");
+        vehiculo.setAnio(scanner.nextInt());
+        scanner.nextLine(); // Consumir el salto de línea pendiente
+
+        System.out.println("Ingrese el número de motor del vehículo:");
+        vehiculo.setNumMotor(scanner.nextLine());
+
+        System.out.println("Ingrese el número de chasis del vehículo:");
+        vehiculo.setChasis(scanner.nextLine());
+
+        System.out.println("Ingrese el color del vehículo:");
+        vehiculo.setColor(scanner.nextLine());
+
+        System.out.println("Ingrese el tipo de vehículo:");
+        vehiculo.setTipo(scanner.nextLine());
+
+        // Crear una instancia de la clase Polizas
+        Polizas poliza = new Polizas();
+
+        // Ingresar los datos de la póliza
+        System.out.println("Ingrese el número de póliza:");
+        poliza.setNumPoliza(scanner.nextLine());
+
+        System.out.println("Ingrese la fecha de inicio de la póliza (formato: dd/MM/yyyy):");
+        String fechaInicioStr = scanner.nextLine();
+        try {
+            Date fechaInicio = new SimpleDateFormat("dd/MM/yyyy").parse(fechaInicioStr);
+            poliza.setFechaInicio(fechaInicio);
+        } catch (ParseException e) {
+            System.out.println("Fecha de inicio inválida. Se utilizará la fecha actual.");
+            poliza.setFechaInicio(new Date());
+        }
+
+        System.out.println("Ingrese la cantidad de cuotas:");
+        poliza.setCuotas(scanner.nextInt());
+        scanner.nextLine(); // Consumir el salto de línea pendiente
+
+        System.out.println("Ingrese la forma de pago:");
+        poliza.setFormaPago(scanner.nextLine());
+
+        System.out.println("Ingrese el monto asegurado:");
+        poliza.setMontoAsegurado(scanner.nextDouble());
+        scanner.nextLine(); // Consumir el salto de línea pendiente
+
+        System.out.println("¿La póliza incluye cobertura de granizo? (true/false):");
+        poliza.setIncluyeGranizo(scanner.nextBoolean());
+        scanner.nextLine(); // Consumir el salto de línea pendiente
+
+        if (poliza.isIncluyeGranizo()) {
+            System.out.println("Ingrese el monto máximo cubierto por granizo:");
+            poliza.setMontoMaxGranizo(scanner.nextDouble());
+            scanner.nextLine(); // Consumir el salto de línea pendiente
+        }
+
+        System.out.println("Ingrese el tipo de cobertura:");
+        poliza.setTipoCobertura(scanner.nextLine());
+
+        // Asignar el cliente y el vehículo a la póliza
+        poliza.setCliente(cliente);
+        poliza.setVehiculo(vehiculo);
+
+        // Mostrar los datos ingresados
+        System.out.println("\nDatos ingresados:");
+        System.out.println(poliza.toString());
     }
-    
 }
