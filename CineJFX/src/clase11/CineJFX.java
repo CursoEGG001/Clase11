c8e2f912e426f85297f35010107f0aef83b52333
CursoEGG001 <soygecko@gmail.com>
2023-05-28 18:18:52 -0300

    No saben que lindo que me quedo el verde ahora!

--- CineJFX/src/clase11/CineJFX.java
+++ CineJFX/src/clase11/CineJFX.java
@@ -4,7 +4,6 @@ package clase11;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
  */
-import com.sun.javafx.css.Style;
 import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
